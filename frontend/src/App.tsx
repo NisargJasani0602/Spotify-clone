161a48930575d62c3fed0d32f9a3744f6d10f2e7
Nisarg Jasani <jasani.nisarg06@gmail.com>
2025-11-19 22:17:41 -0500

    Add NotFoundPage component and update routing to handle 404 errors

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,6 +6,7 @@ import MainLayout from './layout/MainLayout';
 import ChatPage from './pages/Chat/ChatPage';
 import AlbumPage from './pages/album/AlbumPage';
 import AdminPage from './pages/Admin/AdminPage';
+import NotFoundPage from './pages/NotFound404/NotFoundPage';
 
 import { Toaster } from "react-hot-toast";
 
@@ -25,6 +26,7 @@ function App() {
           <Route path="/" element={< Homepage />} />
           <Route path="/chat" element={< ChatPage />} />
           <Route path="/albums/:albumId" element={< AlbumPage />} />
+          <Route path="*" element={< NotFoundPage />} />
         </Route>
       </Routes>
       <Toaster />
